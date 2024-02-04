2f66c57d66906d503acbe284b793946b4dc16d20
PMichelangelo <94895580+PMichelangelo@users.noreply.github.com>
2024-02-04 13:53:51 +0000

    Deploying to gh-pages from @ PMichelangelo/goit-react-hw-05-movies@bca9dc3845205b5b00d01e548feea92ce6e885d0 🚀

---
+++ static/js/794.3a353222.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[794],{794:function(e,s,i){i.r(s);var n=i(87),t=i(184);s.default=function(){return(0,t.jsxs)("div",{children:[(0,t.jsx)("h2",{children:"This page is not found! Try something else!"}),(0,t.jsx)(n.rU,{to:"/",children:"To Home page"})]})}}}]);
+//# sourceMappingURL=794.3a353222.chunk.js.map
